7bc6b6f72fc6c7c15bf7a97f7d93e71678f6c914
Steve <stevewu24@gmail.com>
2025-05-30 12:19:37 -0700

    Updated path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Nav from "./components/Nav";
+import Nav from "./components/nav";
 import "./globals.css";
 
 const geistSans = Geist({
