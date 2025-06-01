562a14ac993dcf43c4a923c6ddb3d13a0b8b18cd
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-31 23:28:19 -0700

    Added more BookCard and Bookinfo component

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,12 +1,18 @@
-import BookCard from "../components/bookcard/BookCard";
-import BookInfo from "../components/bookcard/BookInfo";
+import BookCard from "../components/book/BookCard";
+import BookInfo from "../components/book/BookInfo";
 
 export default function page() {
   return (
     <div>
       <h1>Book Page</h1>
       <BookCard>
-        <BookInfo title={"Percy Jackson and the Lighting Thief"} author={"Rick Riordan"} pages={365} />
+        <BookInfo img={"https://m.media-amazon.com/images/I/81Q54mEEcBL._SL1500_.jpg"} title={"Percy Jackson and the Lighting Thief"} author={"Rick Riordan"} pages={365} />
+      </BookCard>
+      <BookCard>
+        <BookInfo img={"https://m.media-amazon.com/images/I/91YMTyxpWLL._SL1500_.jpg"} title={"The Sea of Monsters"} author={"Rick Riordan"} pages={365} />
+      </BookCard>
+      <BookCard>
+        <BookInfo img={"https://m.media-amazon.com/images/I/91Ca-K4vjOL._SL1500_.jpg"} title={"The Titan's Curse"} author={"Rick Riordan"} pages={365} />
       </BookCard>
     </div>
   )
