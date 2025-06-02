2e0e473f0f1726dcb892865663bf6b4307c0669b
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-01 23:36:57 -0700

    Updated width size

--- app/components/book/BookCard.tsx
+++ app/components/book/BookCard.tsx
@@ -2,7 +2,7 @@ import { ChildrenProps } from "@/app/interfaces/interfaces"
 
 export default function BookCard({ children }: ChildrenProps) {
   return (
-    <div className="relative flex w-56 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+    <div className="relative flex w-64 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
       { children }
     </div>
   )
