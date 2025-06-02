021bc38d44419a9ef1f419e1d9111505dd1816b3
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-01 23:23:30 -0700

    Added styling to Nav, Updated Link component

--- app/components/nav/Nav.tsx
+++ app/components/nav/Nav.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li><Link href={'/'} />Home</li>
-        <li><Link href={'/book'} />Book Page</li>
-        <li><Link href={'/signup'} />Signup</li>
-        <li><Link href={'/login'} />Login</li>
-        <li><Link href={'/logut'}>Logout</Link></li>
+    <nav className="bg-gray-500 p-4">
+      <ul className="flex gap-4">
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/book'}>Book Page</Link></li>
+        <li><Link href={'/signup'}>Signup</Link></li>
+        <li><Link href={'/login'}>Login</Link></li>
+        <li><Link href={'/logout'}>Logout</Link></li>
       </ul>
     </nav>
   )
