968011ecfbf22f306189bb27c4ff7004a2b08a38
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-09 23:03:41 -0700

    Added carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import EmblaCarousel from "./components/embla/EmblaCarousel";
+import { EmblaOptionsType } from 'embla-carousel';
+import "./css/embla.css";
+
+const OPTIONS: EmblaOptionsType = { dragFree: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
 export default function page() {
   return (
     <div>
       <h1>Home Page</h1>
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </div>
-  );
+  )
 }
