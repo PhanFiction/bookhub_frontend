fe263beded4dd2024ac96f0aa24a66f34547012c
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-09 22:57:08 -0700

    Updated interface

--- app/interfaces/interfaces.tsx
+++ app/interfaces/interfaces.tsx
@@ -1,14 +1,22 @@
-export interface BookInfoProps {
-  img: string,
-  title: string,
-  author: string,
-  pages: number
-}
-
 export interface ChildrenProps {
   children: React.ReactNode
 }
 
 export interface HamburgerProps {
   handleToggle: () => void;
+}
+
+// BookDataProps interface represents the structure of book data
+// id, title, author, pages, publisher, isbn, description, published_at, genre, cover_img
+export interface BookDataProps {
+  ID?: number | null;
+  Title?: string;
+  Author?: string;
+  Pages?: number;
+  Publisher?: string;
+  ISBN?: string;
+  Description?: string;
+  PublishedAt?: string;
+  Genre?: string;
+  CoverImage?: string;
 }
