a896869cd8e99f5c2422c2fe14e3561e47fbc062
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-10 22:50:18 -0700

    Updated interface

--- app/interfaces/interfaces.tsx
+++ app/interfaces/interfaces.tsx
@@ -6,17 +6,25 @@ export interface HamburgerProps {
   handleToggle: () => void;
 }
 
+export default interface BookInfoProps {
+  id: number;
+  coverImage: string;
+  title: string;
+  author: string;
+  pages: number;
+}
+
 // BookDataProps interface represents the structure of book data
 // id, title, author, pages, publisher, isbn, description, published_at, genre, cover_img
 export interface BookDataProps {
-  ID?: number | null;
-  Title?: string;
-  Author?: string;
-  Pages?: number;
-  Publisher?: string;
-  ISBN?: string;
-  Description?: string;
-  PublishedAt?: string;
-  Genre?: string;
-  CoverImage?: string;
+  id?: number | null;
+  title?: string;
+  author?: string;
+  pages?: number;
+  Ppublisher?: string;
+  isbn?: string;
+  description?: string;
+  publishedAt?: string;
+  genre?: string;
+  coverImage?: string;
 }
