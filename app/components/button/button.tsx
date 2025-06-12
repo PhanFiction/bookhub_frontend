3c5b761ab23869982d4ae35ac0a430e91fbc9f6d
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-11 23:26:44 -0700

    Created button component

---
+++ app/components/button/button.tsx
@@ -0,0 +1,17 @@
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button = ({ onClick, children }: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
