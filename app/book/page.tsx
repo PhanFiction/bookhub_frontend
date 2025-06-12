790d382e08d84526eacbf035a0b7e1cb286238de
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-11 23:27:26 -0700

    Renamed page and added button component

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,12 +1,21 @@
+'use client'
 import BookCard from "../components/book/BookCard";
 import BookInfo from "../components/book/BookInfo";
 import { fakeBookData } from "../utils/fakeData";
 import BookInfoProps from "../interfaces/interfaces";
+import Button from "../components/button";
 
-export default function page() {
+export default function BookPage() {
   return (
     <div>
       <h1>Book Page</h1>
+
+      {/* Create button to toggle form */}
+      <div className="mb-4">
+        <Button onClick={() => console.log("Add New Book clicked")}>
+          Add New Book
+        </Button>
+      </div>
 
       <div className="flex flex-wrap gap-4">
         {
