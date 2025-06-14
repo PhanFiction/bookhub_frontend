cc9a4e2d1db4a89a4f53100c60c884aa13a9fd3d
Steve <stevewu24@gmail.com>
2025-06-13 23:42:24 -0700

    Added fakeData

---
+++ app/utils/fakeData.tsx
@@ -0,0 +1,276 @@
+import { BookDataProps } from "../interfaces/interfaces";
+
+export const fakeBookData: BookDataProps[] = [
+  {
+    id: 1,
+    title: "The Lightning Thief",
+    author: "Rick Riordan",
+    pages: 377,
+    publisher: "Disney Hyperion",
+    isbn: "9780786838653",
+    description: "Percy Jackson discovers he is a demigod and goes on a quest to recover Zeus's lightning bolt.",
+    publishedAt: "2005-06-28",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91WN6a6F3RL._SY522_.jpg"
+  },
+  {
+    id: 2,
+    title: "The Sea of Monsters",
+    author: "Rick Riordan",
+    pages: 279,
+    publisher: "Disney Hyperion",
+    isbn: "9781423103349",
+    description: "Percy embarks on a quest to retrieve the Golden Fleece to save Camp Half-Blood.",
+    publishedAt: "2006-04-01",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/516i14qMwUL._SY445_SX342_.jpg"
+  },
+  {
+    id: 3,
+    title: "The Titan's Curse",
+    author: "Rick Riordan",
+    pages: 312,
+    publisher: "Disney Hyperion",
+    isbn: "9781423101451",
+    description: "Percy and his friends must rescue Artemis and stop Kronos's forces.",
+    publishedAt: "2007-05-01",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/810lbmYBRAL._SX522_.jpg"
+  },
+  {
+    id: 4,
+    title: "The Battle of the Labyrinth",
+    author: "Rick Riordan",
+    pages: 361,
+    publisher: "Disney Hyperion",
+    isbn: "9781423101468",
+    description: "Percy ventures into Daedalus's Labyrinth to stop Kronos's army.",
+    publishedAt: "2008-05-06",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91qJr8XEyrL._SY522_.jpg"
+  },
+  {
+    id: 5,
+    title: "The Last Olympian",
+    author: "Rick Riordan",
+    pages: 381,
+    publisher: "Disney Hyperion",
+    isbn: "9781423101475",
+    description: "Percy leads the Olympians in a final battle against Kronos.",
+    publishedAt: "2009-05-05",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/817ZfvfwACL._SY522_.jpg"
+  },
+
+  // Heroes of Olympus
+  {
+    id: 6,
+    title: "The Lost Hero",
+    author: "Rick Riordan",
+    pages: 557,
+    publisher: "Disney Hyperion",
+    isbn: "9781423113393",
+    description: "New demigods Jason, Piper, and Leo go on a quest to rescue Hera.",
+    publishedAt: "2010-10-12",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91+s58IKWUL._SX522_.jpg"
+  },
+  {
+    id: 7,
+    title: "The Son of Neptune",
+    author: "Rick Riordan",
+    pages: 521,
+    publisher: "Disney Hyperion",
+    isbn: "9781423140597",
+    description: "Percy awakens at Camp Jupiter with no memory, joining Hazel and Frank on a quest.",
+    publishedAt: "2011-10-04",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/51RdkFy7nDL._SY445_SX342_.jpg"
+  },
+  {
+    id: 8,
+    title: "The Mark of Athena",
+    author: "Rick Riordan",
+    pages: 586,
+    publisher: "Disney Hyperion",
+    isbn: "9781423140603",
+    description: "The seven demigods unite and head to Rome to face Gaea's giants.",
+    publishedAt: "2012-10-02",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/71tgZ0B4jPL._SY522_.jpg"
+  },
+  {
+    id: 9,
+    title: "The House of Hades",
+    author: "Rick Riordan",
+    pages: 597,
+    publisher: "Disney Hyperion",
+    isbn: "9781423146728",
+    description: "Percy and Annabeth journey through Tartarus while the others fight to close the Doors of Death.",
+    publishedAt: "2013-10-08",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/71TooKVJDaL._SY522_.jpg"
+  },
+  {
+    id: 10,
+    title: "The Blood of Olympus",
+    author: "Rick Riordan",
+    pages: 516,
+    publisher: "Disney Hyperion",
+    isbn: "9781423146735",
+    description: "The final battle against Gaea unfolds as the demigods try to prevent her awakening.",
+    publishedAt: "2014-10-07",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91GO+1F0TwL._SY522_.jpg"
+  },
+
+  // Trials of Apollo
+  {
+    id: 11,
+    title: "The Hidden Oracle",
+    author: "Rick Riordan",
+    pages: 376,
+    publisher: "Disney Hyperion",
+    isbn: "9781484732741",
+    description: "Apollo is cast to Earth as a mortal and must restore his godly powers.",
+    publishedAt: "2016-05-03",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/81k4iM9V7kL._SL1500_.jpg"
+  },
+  {
+    id: 12,
+    title: "The Dark Prophecy",
+    author: "Rick Riordan",
+    pages: 401,
+    publisher: "Disney Hyperion",
+    isbn: "9781484746427",
+    description: "Apollo continues his quest to defeat the triumvirate of Roman emperors.",
+    publishedAt: "2017-05-02",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/61cNM0yzvDL._SL1000_.jpg"
+  },
+  {
+    id: 13,
+    title: "The Burning Maze",
+    author: "Rick Riordan",
+    pages: 448,
+    publisher: "Disney Hyperion",
+    isbn: "9781484746434",
+    description: "Apollo battles his way through the Burning Maze to rescue prisoners.",
+    publishedAt: "2018-05-01",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/918ib7TjtwL._SL1500_.jpg"
+  },
+  {
+    id: 14,
+    title: "The Tyrant’s Tomb",
+    author: "Rick Riordan",
+    pages: 448,
+    publisher: "Disney Hyperion",
+    isbn: "9781484746441",
+    description: "Apollo and Meg face off against Tarquin and the undead army.",
+    publishedAt: "2019-09-24",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/81vzGn7vExL._SL1500_.jpg"
+  },
+  {
+    id: 15,
+    title: "The Tower of Nero",
+    author: "Rick Riordan",
+    pages: 416,
+    publisher: "Disney Hyperion",
+    isbn: "9781484746458",
+    description: "Apollo confronts Nero and Python in his final battle as a mortal.",
+    publishedAt: "2020-10-06",
+    genre: "Fantasy, Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/71x3KpjLL3L._SL1500_.jpg"
+  },
+
+  // Magnus Chase (Still in Camp Half-Blood world but Norse)
+  {
+    id: 16,
+    title: "The Sword of Summer",
+    author: "Rick Riordan",
+    pages: 512,
+    publisher: "Disney Hyperion",
+    isbn: "9781423160915",
+    description: "Magnus Chase discovers he’s the son of a Norse god and joins Hotel Valhalla.",
+    publishedAt: "2015-10-06",
+    genre: "Fantasy, Norse Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91VfcPVCd1L._SL1500_.jpg"
+  },
+  {
+    id: 17,
+    title: "The Hammer of Thor",
+    author: "Rick Riordan",
+    pages: 471,
+    publisher: "Disney Hyperion",
+    isbn: "9781423160922",
+    description: "Magnus and friends search for Thor’s missing hammer to prevent Ragnarok.",
+    publishedAt: "2016-10-04",
+    genre: "Fantasy, Norse Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91HYCqr9GoL._SL1500_.jpg"
+  },
+  {
+    id: 18,
+    title: "The Ship of the Dead",
+    author: "Rick Riordan",
+    pages: 410,
+    publisher: "Disney Hyperion",
+    isbn: "9781423160939",
+    description: "Magnus leads his crew to confront Loki and stop the final battle.",
+    publishedAt: "2017-10-03",
+    genre: "Fantasy, Norse Mythology, Adventure",
+    coverImage: "https://m.media-amazon.com/images/I/91R-IaL3LwL._SL1500_.jpg"
+  },
+
+  // Companion books
+  {
+    id: 19,
+    title: "The Demigod Files",
+    author: "Rick Riordan",
+    pages: 160,
+    publisher: "Disney Hyperion",
+    isbn: "9781423121664",
+    description: "Short stories and interviews from Camp Half-Blood.",
+    publishedAt: "2009-02-10",
+    genre: "Fantasy, Companion",
+    coverImage: "https://m.media-amazon.com/images/I/41JZmC-V74L.jpg"
+  },
+  {
+    id: 20,
+    title: "The Demigod Diaries",
+    author: "Rick Riordan",
+    pages: 224,
+    publisher: "Disney Hyperion",
+    isbn: "9781423163008",
+    description: "Additional short stories from the Heroes of Olympus world.",
+    publishedAt: "2012-08-14",
+    genre: "Fantasy, Companion",
+    coverImage: "https://m.media-amazon.com/images/I/91pBHnZq58L._SL1500_.jpg"
+  },
+  {
+    id: 21,
+    title: "Percy Jackson’s Greek Gods",
+    author: "Rick Riordan",
+    pages: 384,
+    publisher: "Disney Hyperion",
+    isbn: "9781423183648",
+    description: "Percy narrates the stories of the Greek gods.",
+    publishedAt: "2014-08-19",
+    genre: "Mythology, Humor",
+    coverImage: "https://m.media-amazon.com/images/I/81UhXCdRLWL.jpg"
+  },
+  {
+    id: 22,
+    title: "Percy Jackson’s Greek Heroes",
+    author: "Rick Riordan",
+    pages: 404,
+    publisher: "Disney Hyperion",
+    isbn: "9781484713931",
+    description: "Percy retells stories of famous Greek heroes.",
+    publishedAt: "2015-08-18",
+    genre: "Mythology, Humor",
+    coverImage: "https://m.media-amazon.com/images/I/911xXYKXF4L.jpg"
+  }
+];
