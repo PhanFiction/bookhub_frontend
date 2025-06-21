f7fb6599e26c247b9def759c8a227ee5d2a0c11c
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-06-20 23:51:00 -0700

    Updated BookInfo component

--- app/components/book/BookInfo.tsx
+++ app/components/book/BookInfo.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import BookInfoProps from "../../interfaces/interfaces";
 
-export default function BookInfo({ coverImage, title, author, pages }: BookInfoProps) {
+export default function BookInfo({ coverImage, title, author, pages, id }: BookInfoProps) {
   return (
     <>
       <div className="relative mx-4 mt-4 h-48 overflow-hidden rounded-xl">
@@ -26,10 +27,12 @@ export default function BookInfo({ coverImage, title, author, pages }: BookInfoP
       {/* Buttons stay at the bottom */}
       <div className="mt-auto flex gap-2 p-6 pt-0">
         <button className="w-full rounded-lg bg-blue-gray-900/10 py-3 px-6 text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 hover:cursor-pointer">
-          Read
+          <Link href={`/book/${id}`}>
+            View
+          </Link>
         </button>
         <button className="w-full rounded-lg bg-blue-gray-900/10 py-3 px-6 text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 hover:cursor-pointer">
-          Remove
+          Save
         </button>
       </div>
     </>
