c77594a981512821c3ece02183553ae6b1504d1b
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-07-02 23:29:19 -0700

    Refactored code

--- app/components/nav/Nav.tsx
+++ app/components/nav/Nav.tsx
@@ -1,33 +1,17 @@
-'use client';
-
 import Link from 'next/link';
-import Hamburger from '../hamburger';
-import { useState } from 'react';
+import SignOut from '@/app/components/auth/SignOut';
+import NavMenu from './NavMenu';
 
 export default function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => setIsOpen(!isOpen);
 
   return (
     <nav className="flex w-full items-center bg-gray-500 p-4">
       <Link href="/">
         <h1 className="text-3xl">BookHub</h1>
       </Link>
-      {/* Hamburger Menu */}
-      <div className="ml-auto">
-        <Hamburger handleToggle={handleToggle} />
-      </div>
-      {
-        isOpen && (
-        <ul className="absolute top-15 right-6 flex flex-col gap-4 bg-black p-4 rounded-md z-1">
-          <li><Link href={'/'}>Home</Link></li>
-          <li><Link href={'/signup'}>Signup</Link></li>
-          <li><Link href={'/login'}>Login</Link></li>
-          <li><Link href={'/logout'}>Logout</Link></li>
-        </ul>
-        )
-      }
+      <NavMenu>
+        <SignOut />
+      </NavMenu>
     </nav>
   )
 }
